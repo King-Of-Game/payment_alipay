c03fb8fda1fc263d7185158163bc06fdf17ee254
YiXuan <43782658+King-Of-Game@users.noreply.github.com>
2021-09-19 08:08:10 +0000

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -7,8 +7,8 @@
     'description': """
         支付宝集成支付
     """,
-    'author': "zwbigdata.com",
-    'website': "http://www.zwbigdata.com",
+    'author': "King-Of-Game",
+    'website': "https://github.com/King-Of-Game/payment_alipay",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
